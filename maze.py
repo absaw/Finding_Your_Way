6e4441dfa47ed8472f5f137a602f76269274646c
Abhishek Sawalkar <abhishek31sawalkar@gmail.com>
2022-12-16 23:26:57 -0500

    way mark 1 achieved

--- maze.py
+++ maze.py
@@ -5,7 +5,6 @@ def generate_maze():
     
     schematic_file=open('Schematics/Schema_1.txt', 'r')
     schema_data=schematic_file.readlines()
-    print(type(schema_data))
     n_row=len(schema_data)
     n_col=len(schema_data[0].strip())
     maze=np.zeros([n_row,n_col])
--- way.py
+++ way.py
@@ -1,23 +1,42 @@
 from maze import *
 
-def way():
+# def way():
+class way
+    def __init__():
+        self.maze=generate_maze()
+        self.n_row=maze.shape[0]
+        self.n_col=maze.shape[1]
+        self.prob_maze=maze.copy()
+        self.prob_maze=initialize_prob(prob_maze)
+        self.prob_maze_new=initialize_prob(prob_maze)
+        # print(prob_maze)
+        self.right=[0, 1]
+        self.left=[0, -1]
+        self.down=[1, 0]
+        self.up=[-1, 0]
 
-    maze=generate_maze()
-    prob_maze=maze.copy()
-    print(prob_maze)
-    right=[0, 1]
-    left=[0, -1]
-    down=[1, 0]
-    up=[-1, 0]
-def initialize_prob(prob_maze):
-    #initialize_prob
-    n_zeros=prob_maze.size-np.count_nonzero(prob_maze)
-    print(n_zeros)
-    for row in range(prob_maze.shape[0]):
-        for col in range(prob_maze.shape[1]):
-            if prob_maze[row][col]==0:
-                prob_maze[row][col]=1/n_zeros
-    return prob_maze
-way()
+    #iterate over rows
+
+    #down operation
+    def down_step(self):
+        for row in range(self.n_row):
+            for col in range(self.n_col):
+                prev_cell=prob_maze+self.down[0]
+                print()
+                # self.prob_maze_new=
+
+    def initialize_prob(self):
+        #initialize_prob
+        
+        n_zeros=prob_maze.size-np.count_nonzero(prob_maze)
+        # print(n_zeros)
+        for row in range(self.prob_maze.shape[0]):
+            for col in range(self.prob_maze.shape[1]):
+                if self.prob_maze[row][col]==0:
+                    self.prob_maze[row][col]=1/n_zeros
+                else:
+                    self.prob_maze[row][col]=0
+        # return self.prob_maze
+# way()
 
 
